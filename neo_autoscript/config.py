5937a0adbed153cfb5ef10c036c208d63397db69
jakovsch <jakovsch@gmail.com>
2021-09-15 18:54:49 +0200

    rewrite: init package structure

---
+++ neo_autoscript/config.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+NEOCONFIRM_URL = 'https://www.minorplanetcenter.net/iau/NEO/toconfirm_tabular.html'
+NEOCONFIRM_CGI = 'https://cgi.minorplanetcenter.net/cgi-bin/confirmeph2.cgi'
+
+class EphemInterval(Enum):
+
+    MINUTES_60 = 0
+    MINUTES_30 = 1
+    MINUTES_10 = 2
+    MINUTES_01 = 3
+
+class Settings:
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    obs_code = 'L01'
+    obs_start = None
+    obs_interval = 5
+    ephem_interval = EphemInterval.MINUTES_10
+    max_mag = 22
+    min_alt = 20
+    min_speed = 10
+    post_open_script = True
+    post_open_excluded = True
